ca687d04bf2187cd78670d03d74713658c61c4bd
atulpathronia <atulpathronia@gmail.com>
2022-06-04 10:53:32 +0530

    finished

--- script.js
+++ script.js
@@ -3,21 +3,11 @@
 const inputs = document.querySelectorAll("input");
 const submitBtn = document.querySelector(".btn");
 
-console.log(inputs);
-
 const validate = function () {
   inputs.forEach((input) => {
     if (!input.value) {
       input.nextElementSibling.classList.remove("hidden");
       input.nextElementSibling.nextElementSibling.classList.remove("hidden");
-    }
-    if (input.name === "email") {
-      if (!input.value.includes("@")) {
-        input.nextElementSibling.classList.remove("hidden");
-        document.querySelector(
-          ".email"
-        ).textContent = `Provide proper email ID`;
-      }
     }
   });
 };
